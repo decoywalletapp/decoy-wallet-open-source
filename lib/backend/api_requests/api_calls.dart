884f18ec84d8d1a1be99bcfef5a3d2bedaf2d4dc
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-10-11 20:14:32 +0000

    go

--- lib/backend/api_requests/api_calls.dart
+++ lib/backend/api_requests/api_calls.dart
@@ -165,7 +165,7 @@ class SetPINCall {
       apiUrl: 'https://vxmrthyumzrfgtuvjqmr.functions.supabase.co/setPin',
       callType: ApiCallType.POST,
       headers: {
-        'Authorization': 'Bearer <Id token (JWT token)>',
+        'Authorization': 'Bearer \${authUserJwtToken}',
         'Content-Type': 'application/json',
       },
       params: {},
@@ -211,7 +211,7 @@ class VerifyPINCall {
       apiUrl: 'https://vxmrthyumzrfgtuvjqmr.functions.supabase.co/verifyPin',
       callType: ApiCallType.POST,
       headers: {
-        'Authorization': 'Bearer <Id token (JWT token)>',
+        'Authorization': 'Bearer \${authUserJwtToken}',
         'Content-Type': 'application/json',
       },
       params: {},
