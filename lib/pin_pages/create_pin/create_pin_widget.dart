df9ca2a608643504f747ed0279639991d7c0a76b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-10-11 22:48:51 +0000

    dollarsign

--- lib/pin_pages/create_pin/create_pin_widget.dart
+++ lib/pin_pages/create_pin/create_pin_widget.dart
@@ -2642,24 +2642,6 @@ class _CreatePinWidgetState extends State<CreatePinWidget> {
                                                         .secondary,
                                               ),
                                             );
-                                            ScaffoldMessenger.of(context)
-                                                .showSnackBar(
-                                              SnackBar(
-                                                content: Text(
-                                                  currentJwtToken,
-                                                  style: TextStyle(
-                                                    color: FlutterFlowTheme.of(
-                                                            context)
-                                                        .primaryText,
-                                                  ),
-                                                ),
-                                                duration: Duration(
-                                                    milliseconds: 4000),
-                                                backgroundColor:
-                                                    FlutterFlowTheme.of(context)
-                                                        .secondary,
-                                              ),
-                                            );
                                             if (SetPINCall.ok(
                                                   (_model.setPinResp
                                                           ?.jsonBody ??
