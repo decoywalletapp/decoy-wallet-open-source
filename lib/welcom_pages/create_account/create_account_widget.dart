cd587f2a75731a1dab7da70c7be31873ae144e1d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-11-02 00:15:59 +0000

    pissed

--- lib/welcom_pages/create_account/create_account_widget.dart
+++ lib/welcom_pages/create_account/create_account_widget.dart
@@ -525,6 +525,21 @@ class _CreateAccountWidgetState extends State<CreateAccountWidget> {
                                 ),
                                 FFButtonWidget(
                                   onPressed: () async {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text(
+                                          'ok',
+                                          style: TextStyle(
+                                            color: FlutterFlowTheme.of(context)
+                                                .primaryText,
+                                          ),
+                                        ),
+                                        duration: Duration(milliseconds: 4000),
+                                        backgroundColor:
+                                            FlutterFlowTheme.of(context)
+                                                .secondary,
+                                      ),
+                                    );
                                     GoRouter.of(context).prepareAuthEvent();
                                     if (_model
                                             .passwordCreateAccountTextController
