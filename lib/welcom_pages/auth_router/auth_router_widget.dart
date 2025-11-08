24f24a0df4c9c4241fa3c1071e021bf3b5e99a78
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-11-08 01:21:37 +0000

    nosncakc

--- lib/welcom_pages/auth_router/auth_router_widget.dart
+++ lib/welcom_pages/auth_router/auth_router_widget.dart
@@ -40,30 +40,6 @@ class _AuthRouterWidgetState extends State<AuthRouterWidget> {
 
     // On page load action.
     SchedulerBinding.instance.addPostFrameCallback((_) async {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            widget.type!,
-            style: TextStyle(
-              color: FlutterFlowTheme.of(context).primaryText,
-            ),
-          ),
-          duration: Duration(milliseconds: 4000),
-          backgroundColor: FlutterFlowTheme.of(context).secondary,
-        ),
-      );
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            widget.token!,
-            style: TextStyle(
-              color: FlutterFlowTheme.of(context).primaryText,
-            ),
-          ),
-          duration: Duration(milliseconds: 4000),
-          backgroundColor: FlutterFlowTheme.of(context).secondary,
-        ),
-      );
       if (widget.type == 'email_change') {
         if (widget.token != '') {
           _model.verifyEmailResp = await SupabaseVerifyEmailChangeCall.call(
