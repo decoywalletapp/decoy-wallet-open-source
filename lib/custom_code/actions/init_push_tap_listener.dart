62c0ff60b009f61f25b8ab269480f57c9f3bbfcf
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-02-22 19:53:55 +0000

    routererrr

--- lib/custom_code/actions/init_push_tap_listener.dart
+++ lib/custom_code/actions/init_push_tap_listener.dart
@@ -9,35 +9,57 @@ import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
-import 'dart:async';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-bool _tapListenerInitialized = false;
-String? _lastPushRoute;
+bool _pushTapListenerInitialized = false;
 
 Future<String?> initPushTapListener(BuildContext context) async {
-  try {
-    if (_tapListenerInitialized) {
-      return _lastPushRoute;
-    }
-    _tapListenerInitialized = true;
-
-    // If app was opened by tapping a notification while terminated
-    final initialMessage = await FirebaseMessaging.instance.getInitialMessage();
-    if (initialMessage != null) {
-      final type = initialMessage.data['type']?.toString();
-      _lastPushRoute = type;
-      return _lastPushRoute;
-    }
-
-    // If app is in background and user taps notification
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      final type = message.data['type']?.toString();
-      _lastPushRoute = type;
-    });
-
-    return _lastPushRoute;
-  } catch (e) {
+  // You can call this action on multiple pages; it will only wire listeners once.
+  if (_pushTapListenerInitialized) {
     return null;
   }
+  _pushTapListenerInitialized = true;
+
+  String? routeFromMessage(RemoteMessage message) {
+    final data = message.data;
+    final type = data['type']?.toString();
+
+    // This must match what you send in sendPush:
+    // type: "entitlement_renewal_reminder"
+    if (type == 'entitlement_renewal_reminder') {
+      return 'renew_btcpay';
+    }
+
+    return null;
+  }
+
+  void handleRoute(String route) {
+    // Always force the user back through your auth flow (Face ID + PIN)
+    // Then Home will redirect to Manage Subscriptions based on app state.
+    try {
+      context.goNamed('AuthRouter');
+    } catch (_) {
+      // If routing fails for any reason, do nothing (avoid crashing).
+    }
+  }
+
+  // Cold start: app launched from terminated by tapping push
+  final initial = await FirebaseMessaging.instance.getInitialMessage();
+  if (initial != null) {
+    final r = routeFromMessage(initial);
+    if (r != null) {
+      handleRoute(r);
+      return r;
+    }
+  }
+
+  // Background: app opened by tapping push
+  FirebaseMessaging.onMessageOpenedApp.listen((message) {
+    final r = routeFromMessage(message);
+    if (r != null) {
+      handleRoute(r);
+    }
+  });
+
+  return null;
 }
