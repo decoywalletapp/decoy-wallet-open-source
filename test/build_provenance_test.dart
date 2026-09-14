c4b3dee22cfbc78eeca1ae3bc49dc8f3cfed6d42
decoywalletapp <founder@decoywalletapp.com>
2026-09-14 18:28:11 -0400

    Update provenance test for 1.1.0 RC

--- test/build_provenance_test.dart
+++ test/build_provenance_test.dart
@@ -12,6 +12,6 @@ void main() {
     expect(DecoyBuildProvenance.hasSourceCommit, isFalse);
     expect(DecoyBuildProvenance.shortCommit, 'unknown');
     expect(DecoyBuildProvenance.commitUrl, isNull);
-    expect(DecoyBuildProvenance.versionLabel, '1.0.5+10005');
+    expect(DecoyBuildProvenance.versionLabel, '1.1.0+11000');
   });
 }
