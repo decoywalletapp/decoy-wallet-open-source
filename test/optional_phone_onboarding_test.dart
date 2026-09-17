ef573cd68e2b16615fa0c58d8bef20a974674686
decoywalletapp <founder@decoywalletapp.com>
2026-09-17 19:43:45 -0400

    Revert "Fix phone input back navigation"
    This reverts commit b45179a0cb3cd62083ce4b8ca448053b86c1b0a6.

--- test/optional_phone_onboarding_test.dart
+++ test/optional_phone_onboarding_test.dart
@@ -39,10 +39,7 @@ void main() {
 
     expect(phoneInput, contains("'feature': 'sms_terms'"));
     expect(phoneInput, contains("'accepted_sms_terms': true"));
-    expect(
-      phoneInput,
-      matches(RegExp(r'PhoneNumberVerificationWidget\s*\.routeName')),
-    );
+    expect(phoneInput, contains('PhoneNumberVerificationWidget.routeName'));
     expect(phoneInput, contains('See SMS Terms and Privacy Policy.'));
 
     final verification = File(
@@ -153,13 +150,6 @@ void main() {
     expect(phoneInput, contains('final String? initialPhone'));
     expect(phoneInput,
         contains("TextEditingController(text: widget.initialPhone ?? '')"));
-    expect(
-      phoneInput,
-      matches(RegExp(r'PhoneNumberVerificationWidget\s*\.routeName')),
-    );
-    expect(phoneInput, contains('if (widget.initialPhone != null)'));
-    expect(phoneInput, contains('Icons.arrow_back_rounded'));
-    expect(phoneInput, contains('PersonalInformationWidget.routeName'));
-    expect(phoneInput, isNot(contains("text: 'Back to Login'")));
+    expect(phoneInput, contains('PhoneNumberVerificationWidget.routeName'));
   });
 }
